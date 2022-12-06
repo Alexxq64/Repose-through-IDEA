3a0eadc5132ca1a9aed3445cadf6b988afb52b06
Alex Kuznetsov <alexxq@list.ru>
2022-12-06 16:50:07 +0300

    All done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Separator sp = new Separator(new int[] {1, 6, 2, 4, 7, 9, 24, 58});
-        System.out.println("Hello world!");
+        System.out.println("Hello!");
     }
 }
